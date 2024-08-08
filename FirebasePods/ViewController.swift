e5d7e233e7ca04101cd833b268463a47a87859a2
Waleed <Muhammadwaleed@contentarcade.com>
2024-08-08 10:48:52 +0500

    Initial commit

---
+++ FirebasePods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebasePods
+//
+//  Created by MacBook on 08/08/2024.
+//
+
+import UIKit
+import GoogleMobileAds
+
+class ViewController: UIViewController {
+    var inter:GADInterstitial!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FirebasePods' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FirebasePods
+  pod 'Google-Mobile-Ads-SDK', '~> 7.69.0'
+end
